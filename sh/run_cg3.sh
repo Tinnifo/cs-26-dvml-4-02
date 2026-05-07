5859a5c80387915099d35d8328c36f7f79ef3b68
CodeLearnerDC <AB10IX@student.aau.dk>
2026-05-08 01:25:47 +0200

    fixed and added sh to cg3

---
+++ sh/run_cg3.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name=cg3-grid
+#SBATCH --partition=prioritized
+#SBATCH --account=aau
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=15
+#SBATCH --mem=24G
+#SBATCH --time=24:00:00
+#SBATCH --output=logs/cg3_%j.out
+
+set -e
+source .venv/bin/activate
+
+export TORCH_FORCE_NO_WEIGHTS_ONLY_LOAD=1
+
+
+echo "[CG3] running combinations..."
+
+python3 src/train.py --multirun +experiment=cg3_combinations
