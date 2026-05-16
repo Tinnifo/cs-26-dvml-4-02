698b37f995a2f90230c52a1b3ed48bace689ce9b
CodeLearnerDC <AB10IX@student.aau.dk>
2026-05-16 23:46:34 +0200

    fixed

--- sh/run_baselines.sh
+++ sh/run_baselines.sh
@@ -4,7 +4,7 @@
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=15
 #SBATCH --mem=32G
-#SBATCH --time=18:00:00
+#SBATCH --time=16:00:00
 #SBATCH --output=logs/%x-%j.out
 #SBATCH --error=logs/%x-%j.err
 
