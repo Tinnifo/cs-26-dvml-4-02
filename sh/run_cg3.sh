ef51f1de3c2079f9fc7b93f6849da4300818670a
CodeLearnerDC <AB10IX@student.aau.dk>
2026-05-19 01:44:36 +0200

    percentage budget

--- sh/run_cg3.sh
+++ sh/run_cg3.sh
@@ -4,7 +4,7 @@
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=32G
-#SBATCH --time=14:00:00
+#SBATCH --time=12:00:00
 #SBATCH --output=logs/%x-%A_%a.out
 #SBATCH --error=logs/%x-%A_%a.err
 #SBATCH --array=0-2
