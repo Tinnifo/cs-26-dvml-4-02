e0507bb6b55fc1156afd4305d1dffc89ee876cbc
CodeLearnerDC <AB10IX@student.aau.dk>
2026-05-21 01:50:56 +0200

    fixed config for baselines

--- sh/run_baselinesPC.sh
+++ sh/run_baselinesPC.sh
@@ -45,7 +45,7 @@ DATASET=${DATASETS[$SLURM_ARRAY_TASK_ID]}
 echo "Running dataset: $DATASET"
 
 python3 src/train.py --multirun \
-    model=gin,sage,gt \
+    model=gcn,gat,gin,sage,gt \
     method=vanilla,iceberg \
     dataset=$DATASET \
     label_strategy=per_class \
