cb430f5d1bc1e43b11fb9a2d4151005fe0a7d349
CodeLearnerDC <AB10IX@student.aau.dk>
2026-05-22 03:22:38 +0200

    fix gin

--- src/train.py
+++ src/train.py
@@ -336,8 +336,8 @@ def main(cfg: DictConfig) -> float:
     #master_csv_path = os.path.join(get_original_cwd(), "all_experimentsBaselinesPC.csv")    
     #master_csv_path = os.path.join(get_original_cwd(), "all_experimentsBaselinesPB.csv")    
 
-    master_csv_path = os.path.join(get_original_cwd(), "all_experimentsPerClass.csv")
-    #master_csv_path = os.path.join(get_original_cwd(), "all_experimentsCG3Percentage.csv")
+    #master_csv_path = os.path.join(get_original_cwd(), "all_experimentsPerClass.csv")
+    master_csv_path = os.path.join(get_original_cwd(), "all_experimentsCG3Percentage.csv")
 
 
 
